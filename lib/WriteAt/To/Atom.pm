e0612d9d209d5fcc5c6f491d54ec838e30af902b
Aliaksandr Zahatski <github.send@zag.ru>
2012-11-24 08:36:23 +0400

    add Atom to

---
+++ lib/WriteAt/To/Atom.pm
@@ -0,0 +1,12 @@
+#===============================================================================
+#
+#  DESCRIPTION:  convert pod6 to atom
+#
+#       AUTHOR:  Aliaksandr P. Zahatski, <zahatski@gmail.com>
+#===============================================================================
+package Writeat::To::Atom;
+use strict;
+use warnings;
+
+1;
+
